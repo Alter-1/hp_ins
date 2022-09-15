5226b4e1be03069d2eb8ebd28faeab1fa39a3d0e
Alexandr A. Telyatnikov (Alter) <alter@alter.org.ua>
2022-09-15 14:32:17 +0300

    fix install script

--- build-install.sh
+++ build-install.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 apt install libevdev-dev
 gcc -Wall `pkg-config libevdev --cflags` -o hp_ins hp_ins.c `pkg-config libevdev --libs`
-cp hp_ins /ust/local/bin
+cp hp_ins /usr/local/bin
 cp hp_ins.service /etc/systemd/system
 systemctl start hp_ins
 systemctl enable hp_ins
